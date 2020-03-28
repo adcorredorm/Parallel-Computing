3f061e82fd3dd0015f035a003fd548113401d5fb
Angel Corredor <adcorredorm@unal.edu.co>
2020-03-28 15:14:31 -0500

    omp_pi final form

--- Exercices/omp_pi.c
+++ Exercices/omp_pi.c
@@ -5,23 +5,13 @@
 #define CICLES 1e09
 
 void main(){
-    double pi_sub[THREADS];
-    #pragma omp parallel num_threads(THREADS)
-    {
-        int id = omp_get_thread_num();
-        int init = id * CICLES/THREADS;
-        int end = (id + 1) * CICLES/THREADS;
-        int i = init;
-        do{
-            pi_sub[id] += (double)(4.0 / (2*i+1));
-            i++;
-            pi_sub[id] -= (double)(4.0 / (2*i+1));
-            i++;
-        }while(i < end);
-    }
-    double pi = 0.0;
-    for(int i = 0; i < THREADS; i++){
-        pi += pi_sub[i];
-    }
-    printf("Pi: %0.20f\n", pi);
+   double pi = 0.0;
+   int it = CICLES;
+   #pragma omp parallel for reduction(+:pi) num_threads(THREADS)
+   for(int i = 0; i < it; i++){
+       pi = pi + (double)(4.0 / (2*i+1));
+       i++;
+       pi = pi - (double)(4.0 / (2*i+1));
+   }
+   printf("Pi: %0.20f\n", pi);
 }
