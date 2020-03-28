f533bd1decd69001bdee05876e8dcb896d3b0359
Angel Corredor <adcorredorm@unal.edu.co>
2020-03-28 13:56:41 -0500

    Margins Done!

--- BlurEffect/main.c
+++ BlurEffect/main.c
@@ -3,7 +3,10 @@
 #include <png.h>
 #include "image_load.h"
 
-int kernel_size, **kernel;
+#define max(a,b) ((a) > (b) ? (a) : (b))
+#define min(a,b) ((a) < (b) ? (a) : (b))
+
+int kernel_size, padding, **kernel;
 
 void create_kernel(){
   kernel = malloc( kernel_size * sizeof(int *));
@@ -13,31 +16,36 @@ void create_kernel(){
     if(kernel[i] == NULL) return;
     for(int j = 0; j < kernel_size; j++) kernel[i][j] = 1;
   }
+  padding = kernel_size / 2;
 }
 
-png_bytep calculate(int x, int y){
-  int sum_r = 0, sum_g = 0, sum_b = 0;
+void calculate(int x, int y){
   png_bytep px, current_px;
-  for(int i = y-(kernel_size/2); i < y+(kernel_size/2); i++){
-    for(int j = x-(kernel_size/2); j < x+(kernel_size/2); j++){
+  int sum_r = 0, sum_g = 0, sum_b = 0, pixels = 0;
+  int init_i = max(0, y - padding), end_i = min(height - 1, y + padding);
+  int init_j = max(0, x - padding), end_j = min(width - 1, x + padding);
+  
+  for(int i = init_i; i <= end_i; i++){
+    for(int j = init_j; j <= end_j; j++){
       current_px = &(row_pointers[i][j*4]);
       sum_r = sum_r + current_px[0];
       sum_g = sum_g + current_px[1];
       sum_b = sum_b + current_px[2];
+      pixels += 1;
     }
   }
 
   px = &(row_pointers[y][x*4]);
     
-  px[0] = sum_r /(kernel_size*kernel_size);
-  px[1] = sum_g /(kernel_size*kernel_size);
-  px[2] = sum_b /(kernel_size*kernel_size);
+  px[0] = sum_r /pixels;
+  px[1] = sum_g /pixels;
+  px[2] = sum_b /pixels;
 
 }
 
 void process_png_file() {
-  for(int y = kernel_size; y < height-kernel_size; y++) {
-    for(int x = kernel_size; x < width-kernel_size; x++) {
+  for(int y = 0; y < height; y++) {
+    for(int x = 0; x < width; x++) {
       calculate(x, y);
     }
   }
@@ -48,7 +56,7 @@ int main(int argc, char *argv[]) {
   if(argc != 3) abort();
 
   read_png_file(argv[1]);
-  kernel_size = 20;
+  kernel_size = 15;
   create_kernel();
   process_png_file();
   write_png_file(argv[2]);
