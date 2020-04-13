74e5a55e0a163692d6f19affc7daa17980312baf
Sebastian <sfdelgadop@unal.edu.co>
2020-04-13 15:54:44 -0500

    adding 1 core option

--- BlurEffect/Run.sh
+++ BlurEffect/Run.sh
@@ -10,6 +10,11 @@ echo "Experiments with a kernel of" $KERNEL >Statistics.txt
 #720P image
 echo '---------------------------------------------' >> Statistics.txt
 echo '720P Image' >> Statistics.txt
+
+#Blur effect with 1 threads 
+./blur-effect 720.png 720Result.png $KERNEL 1 >> Statistics.txt
+rm -f 720Result.png
+
 #Blur effect with 2 threads 
 ./blur-effect 720.png 720Result.png $KERNEL 2 >> Statistics.txt
 rm -f 720Result.png
@@ -29,6 +34,10 @@ echo '---------------------------------------------' >> Statistics.txt
 #1080P image
 echo '1080P Image' >> Statistics.txt
 
+#Blur effect with 1 threads 
+./blur-effect 1080.png 1080Result.png $KERNEL 1 >> Statistics.txt
+rm -f 1080Result.png
+
 #Blur effect with 2 threads 
 ./blur-effect 1080.png 1080Result.png $KERNEL 2 >> Statistics.txt
 rm -f 1080Result.png
@@ -47,6 +56,10 @@ echo '---------------------------------------------' >> Statistics.txt
 
 #4k image
 echo '4k Image' >> Statistics.txt
+
+#Blur effect with 1 threads 
+./blur-effect 4k.png 4kResult.png $KERNEL 1 >> Statistics.txt
+rm -f 4kResult.png
 
 #Blur effect with 2 threads 
 ./blur-effect 4k.png 4kResult.png $KERNEL 2 >> Statistics.txt
